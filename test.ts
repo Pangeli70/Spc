377cd7a422e95d9d9f2f95c947cbf99d647c25b6
PAngeli70 <pangeli70@gmail.com>
2024-12-24 14:30:31 +0100

    Final aestethics

--- test.ts
+++ test.ts
@@ -6,9 +6,12 @@
  * @version 1.0.0 [APG 2024/09/21] Moving to Deno 2
  * ------------------------------------------------------------------------
  */
-import {Spc} from "./test/deps.ts";
-import {ApgSpc_Spec_ApgUts_Math} from "./test/specs/ApgSpc_Spec_ApgUts_Math.ts";
-import {ApgSpc_Spec_ApgUts_Object} from "./test/specs/ApgSpc_Spec_ApgUts_Object.ts";
+
+
+import { Spc } from "./test/deps.ts";
+import { ApgSpc_Spec_ApgUts_Math } from "./test/specs/ApgSpc_Spec_ApgUts_Math.ts";
+import { ApgSpc_Spec_ApgUts_Object } from "./test/specs/ApgSpc_Spec_ApgUts_Object.ts";
+
 
 // Todo This is deprecated sooner or later remove it --APG 20241224
 // Remote test result browser service address 
@@ -18,42 +21,48 @@ const RESULTS_BROWSER_URI = "https://apg-tst.deno.dev/store";
 const FRAMEWORK = "ApgSpc";
 
 
+
 /** 
  * Use this test suite as an example to understand how to use the Spc framework
  * @param arun Include or exclude the entire test suite
  */
 async function ApgSpc_Suite(arun: Spc.ApgSpc_eRun) {
 
-    const results: Spc.ApgSpc_TSpecResult[] = [];
-
     if (arun != Spc.ApgSpc_eRun.yes) return;
 
-    const ApgUts_Object_Spec = new ApgSpc_Spec_ApgUts_Object();
 
+    const results: Spc.ApgSpc_TSpecResult[] = [];
+
+    const ApgUts_Object_Spec = new ApgSpc_Spec_ApgUts_Object();
     const r1 = await ApgUts_Object_Spec.Run(Spc.ApgSpc_eRun.yes)
 
     if (r1) {
+
         results.push(Spc.ApgSpc_Service.Result());
+
         const r = await Spc.ApgSpc_Service.SendEventsToResultsBrowser(
             RESULTS_BROWSER_URI,
             FRAMEWORK,
             ApgUts_Object_Spec.NAME
         );
+
         Spc.ApgSpc_Service.Reset();
     }
 
 
     const ApgUts_Math_Spec = new ApgSpc_Spec_ApgUts_Math();
-
     const r2 = await ApgUts_Math_Spec.Run(Spc.ApgSpc_eRun.yes)
 
     if (r2) {
+
         results.push(Spc.ApgSpc_Service.Result());
+        
         const r = await Spc.ApgSpc_Service.SendEventsToResultsBrowser(
             RESULTS_BROWSER_URI,
             FRAMEWORK,
             ApgUts_Math_Spec.NAME
         );
+
         Spc.ApgSpc_Service.Reset();
     }
 
